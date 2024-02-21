d0dd3f60c6527b50d0d8980b945f00333ee48ddd
choiru <c.zain@latrobe.edu.au>
2024-02-21 22:44:40 +1100

    solution

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,25 @@
+function Task(props) {
+	return (
+		<li>{ props.description } <input type="checkbox" checked={props.completed} readOnly /></li>
+	);
+}
+
 function List(props) {
+
+	//// Old For Loop
+	// let tasks = [];
+	// for (let i = 0; i < props.tasks.length; i++) {
+	// 	let description = props.tasks[i].description;
+	// 	let completed = props.tasks[i].completed;
+	// 	tasks.push(<Task description={description} completed={completed} />);
+	// }
+
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				{/* { tasks } */}
+				{ props.tasks.map(task => <Task description={task.description} completed={task.completed} />) }
 			</ul>
 		</div>
 	);
